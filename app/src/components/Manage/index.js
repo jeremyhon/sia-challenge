4667300c255ad72fa6244fcab9bc0109b3c17530
Jeremy Hon <jeremy.hon.gy@gmail.com>
2017-10-14 15:28:07 -0700

    Merge

--- app/src/components/Manage/index.js
+++ app/src/components/Manage/index.js
@@ -1,18 +1,66 @@
 import React, { Component } from "react";
 import _ from "lodash";
+import SeatMap from "../SeatMap";
+import "./index.scss";
 
 class Manage extends Component {
+  renderFailures() {
+    const failures = _.filter(data, { status: "failure" });
+    return _.map(failures, (item, index) => (
+      <li key={index}>
+        {item.type}:{item.number}
+      </li>
+    ));
+  }
+
+  renderAttention() {
+    const watch = _.filter(data, { status: "watch" });
+    return _.map(watch, (item, index) => (
+      <li key={index}>
+        {item.type}:{item.number}
+      </li>
+    ));
+  }
+
+  renderRows() {
+    return _.times(30, () => row);
+  }
+
   render() {
     return (
       <div>
-        <h1>Flight defects</h1>
-        <h2>Needs fixing</h2>
-        <ul />
-        <h2>Needs attention</h2>
+        <h1 className="title">Flight defects</h1>
+        <div className="flex-container">
+          <div className="col seatmap-col">
+            <SeatMap rows={this.renderRows()} maxReservableSeats={0} />
+          </div>
+          <div className="col list-col">
+            <div className="danger">
+              <h2>Needs fixing</h2>
+              <ul>{this.renderFailures()}</ul>
+            </div>
+            <div className="attention">
+              <h2>Needs attention</h2>
+              <ul>{this.renderAttention()}</ul>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
+
+const numberOfRows = 30;
+
+const row = [
+  { number: "A" },
+  { number: "B" },
+  { number: "C" },
+  null,
+  { number: "G" },
+  { number: "H" },
+  { number: "J" }
+];
 
 const data = [
   {
